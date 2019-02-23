d8f881c0804951d16ae2e551e9d3f6c6748a16a7
Rangot <arty-a.2017@mail.ru>
2019-02-23 20:00:50 +0300

    tag detail and post detail created

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-from models import Post
+from models import Post, Tag
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
@@ -8,11 +8,23 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/')
 def index():
-    posted = Post.query.all()
+    q = request.args.get('q')
+    if q:
+        posted = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+    else:
+        posted = Post.query.all()
     return render_template('posts/index.html', posted=posted)
 
 
 @posts.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter(Post.slug == slug).first()
-    return render_template('posts/post_detail.html', post=post)
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
+
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    posted = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posted)
