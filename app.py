614c7fa25a50a6e5dfe4a30c471892b4a67f5b3f
Rangot <arty-a.2017@mail.ru>
2019-02-28 00:31:20 +0300

    login_user added: macros, menu, messages

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 
 from flask_security import SQLAlchemySessionUserDatastore
-from flask_security import Security
+from flask_security import Security, current_user
 
 
 app = Flask(__name__)
@@ -24,9 +24,42 @@ manager.add_command('db', MigrateCommand)
 
 # ADMIN
 from models import *
-admin = Admin(app)
-admin.add_view(ModelView(Post, db.session))
-admin.add_view(ModelView(Tag, db.session))
+
+
+class AdminMixin(ModelView):
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
+
+
+# Чтобы slug создавался при создании экземпляра через админку
+class BaseModelView(ModelView):
+    def on_model_change(self, form, model, is_created):
+        model.generate_slug()
+        return super(BaseModelView, self).on_model_change(form, model, is_created)
+
+
+class AdminView(AdminMixin, ModelView):
+    pass
+
+
+class HomeAdminView(AdminMixin, AdminIndexView):
+    pass
+
+
+class PostAdminView(AdminMixin, BaseModelView):
+   form_columns = ['title', 'body']
+
+
+class TagAdminView(AdminMixin, BaseModelView):
+    form_columns = ['name']
+
+
+admin = Admin(app, 'FlaskApp')
+admin.add_view(PostAdminView(Post, db.session))
+admin.add_view(TagAdminView(Tag, db.session))
 # admin.add_view(ModelView(User, db.session))
 # admin.add_view(ModelView(Role, db.session))
 
